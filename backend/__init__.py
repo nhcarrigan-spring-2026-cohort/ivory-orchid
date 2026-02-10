33d5738e8145f72d1522124c2ed0157726835b6a
Matita008 <155455608+Matita008@users.noreply.github.com>
2026-02-10 02:20:12 +0100

    Create /data endpoints (/data/pets and /data/shelters) (#27)
    * created /pets and /shelters endpoints with placeholder data, added .jpg mapping in static.py
    
    * change endpoints to not collide with the shelters and pets pages
    
    * Added specie to the placeholder data
    
    * small fix
    There was a missing name
    
    * Added comments

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,4 +4,5 @@ app = Flask(__name__, template_folder="../frontend/templates")
 
 from backend import static
 #Import here all the files that contain at least an endpoint
-from backend import test
+from backend import dataEndpoints
+app.register_blueprint(dataEndpoints.data_bp)
