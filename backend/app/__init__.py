7c45373a2cd6863f7897ea97c7c08a91cc8cd6db
Matita008 <155455608+Matita008@users.noreply.github.com>
2026-02-24 00:00:56 +0100

    Implemented the database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__, template_folder="../../frontend/templates")
+db = SQLAlchemy()
+
+#Sets various config from env vars or defaults
+if os.getenv("SQLALCHEMY_DATABASE_URI") and os.getenv("SQLALCHEMY_DATABASE_URI").strip() != "":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.abspath(os.path.join(os.path.dirname(__file__), "..")) + "/database.db"
+
+#Import database models
+from . import types, utils
+
+db.init_app(app)
+
+def setup_db():
+    first = types.Shelter.query.get(1)
+
+#Create the tables if they are missing
+with app.app_context():
+    utils.setup_db(db)
 
 #Import here all the files that contain at least an endpoint
 from . import dataEndpoints, static
--- backend/app/dataEndpoints.py
+++ backend/app/dataEndpoints.py
@@ -4,28 +4,6 @@ from .types import *
 
 data_bp = Blueprint('data', __name__, url_prefix='/api')
 
-USE_DUMMY_VALUES = True #until the db is implemented
-
-if USE_DUMMY_VALUES:
-	def get_pets() -> list[Pet]:
-		dog1 = Pet("rex", 10, "male", "calm border-collie", "kripto.jpg", 6.25, "cohort", True, "dog")
-		dog2 = Pet("max", 3, "male", "survived a car crash", "dogandcat.jpg", 2.1, "shelterB", True, "dog")
-		dog3 = Pet("bella", 1, "female", "loves big walks, needs attention", "kripto.jpg", 6.25, "cohort", True, "dog")
-
-		cat1 = Pet("Snowball", 10, "male", "Full of energy", "dogandcat.jpg", 15, "shelterB", True, "cat")
-		cat2 = Pet("fog", 10, "female", "has an interesting color", "snowball.jpg", 3, "cohort", True, "cat")
-		cat3 = Pet("sunny", 10, "female", "Very fat", "dogandcat.jpg", 8, "closedShelter", False, "cat")
-
-		return [dog1, dog2, dog3, cat1, cat2, cat3]
-
-	def get_shelters() -> list[Shelter]:
-		shelter_cohort = Shelter("cohort", "ivory-orchid@cohort.org", "+156547896542", "12 rue de Prony, 75017 Paris, France")
-		shelter_b = Shelter("shelterB", "shelters@ivory.orchid.org", "+215654789564", "760 United Nations Plaza, New York, NY 10017, USA")
-		shelter_closed = Shelter("closedShelter", "automations@example.com","+0123456789","1600 Pennsylvania Ave NW, Washington, DC 20500, USA")
-
-		return [shelter_cohort, shelter_b, shelter_closed]
-	#endif USE_DUMMY_VALUES
-
 @data_bp.route('/pets', methods = ['GET'])
 def full_pets_list():
 	return Response(jsonify(get_pets(), JsonType.Short), mimetype="application/json")
